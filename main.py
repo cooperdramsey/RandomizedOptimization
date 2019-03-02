e29621918739b2ade8d763aa549ce09f95a9ddf6
Cooper Ramsey <32044877+cooperdramsey@users.noreply.github.com>
2019-03-02 11:19:22 -0700

    Update README format

---
+++ main.py
@@ -0,0 +1,11 @@
+# Apply the following three algorithms to train weights on a Neural Network from assignment 1.
+# Randomized Hill Climbing
+# Simulated Annealing
+# Genetic algorithm
+# MIMIC
+
+# From assignment 1, I used Grid searches to find the best parameters. For the sake of consistency, I will use the
+# exact same neural network in this assignment so I can directly compare the performance of the various algorithms to
+# back propagation.
+
+
