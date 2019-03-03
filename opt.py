4764ad505cb08487419606fc775f2e4fd42dfcbc
Cooper Ramsey <cooperdramsey@gmail.com>
2019-03-02 21:50:19 -0700

    Rename main file to nn for neural net

---
+++ opt.py
@@ -0,0 +1,11 @@
+# Optimization Example Problems
+
+# One Max
+
+# Flip Flop
+
+# Four Peaks Problem (Simulated Annealing example)
+
+# K-Color Problem (MIMIC example)
+
+# Flip Flop with adjusted fitness function
